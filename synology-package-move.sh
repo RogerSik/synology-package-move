e027ca462adf12c9a173dfbe51ec88e1efdca2b1
Roger Sikorski <roger@sikorski.cloud>
2024-02-01 19:28:08 +0100

    disable synology-package-move.sh

--- synology-package-move.sh
+++ synology-package-move.sh
@@ -1,30 +1,33 @@
-#!/bin/bash
+# TODOS
+# need to pass more directories, see at synology-package-move-all-packages.sh
 
-# Check if the parameter is provided
-if [ -z "$1" ]; then
-  echo "Please provide following arguments:"
-  echo "package_name source_volume destination_volume"
-  echo "Example ./package-move.sh HyperBackup volume1 volume2"
-  exit 1
-fi
+# #!/bin/bash
 
-PACKAGE_NAME=$1
-SOURCE_VOLUME=$2
-DEST_VOLUME=$3
+# # Check if the parameter is provided
+# if [ -z "$1" ]; then
+#   echo "Please provide following arguments:"
+#   echo "package_name source_volume destination_volume"
+#   echo "Example ./package-move.sh HyperBackup volume1 volume2"
+#   exit 1
+# fi
 
-echo "Stop the package $PACKAGE_NAME"
-synopkg stop "$PACKAGE_NAME"
+# PACKAGE_NAME=$1
+# SOURCE_VOLUME=$2
+# DEST_VOLUME=$3
 
-echo "Move package from $SOURCE_VOLUME to $DEST_VOLUME"
-mv "/$SOURCE_VOLUME/@appstore/$PACKAGE_NAME" "/$DEST_VOLUME/@appstore/$PACKAGE_NAME"
+# echo "Stop the package $PACKAGE_NAME"
+# synopkg stop "$PACKAGE_NAME"
 
-echo "Remove the existing target"
-rm -f "/var/packages/$PACKAGE_NAME/target"
+# echo "Move package from $SOURCE_VOLUME to $DEST_VOLUME"
+# mv "/$SOURCE_VOLUME/@appstore/$PACKAGE_NAME" "/$DEST_VOLUME/@appstore/$PACKAGE_NAME"
 
-echo "Create a symbolic link to the new location"
-ln -s "/$DEST_VOLUME/@appstore/$PACKAGE_NAME" "/var/packages/$PACKAGE_NAME/target"
+# echo "Remove the existing target"
+# rm -f "/var/packages/$PACKAGE_NAME/target"
 
-echo "Start the package $PACKAGE_NAME"
-synopkg start "$PACKAGE_NAME"
+# echo "Create a symbolic link to the new location"
+# ln -s "/$DEST_VOLUME/@appstore/$PACKAGE_NAME" "/var/packages/$PACKAGE_NAME/target"
 
-echo "Move of $PACKAGE_NAME completed."
+# echo "Start the package $PACKAGE_NAME"
+# synopkg start "$PACKAGE_NAME"
+
+# echo "Move of $PACKAGE_NAME completed."
